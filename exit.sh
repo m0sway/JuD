f4fbab17999bb98760befdca0fc1e71a84eef49f
m0sway <the.m0s.way@gmail.com>
2021-06-03 21:22:35 +0800

    clear

--- exit.sh
+++
@@ -1,6 +0,0 @@
-ps -ef |grep subdomain |awk '{print $2}'|xargs sudo kill -9
-ps -ef |grep xray |awk '{print $2}'|xargs sudo kill -9
-sudo cat ./results/result.sqlite3 > ./results/$(date "+%Y%m%d%H%M%S")_result.sqlite3
-sudo rm -rf ./results/result.sqlite3
-sudo rm -rf ./results/results.sqlite3
-sudo rm -rf ./tools/OneForAll/results/*
